87ff94d99f49059609e411bf4547fdd880052631
Yashaswini Singh Shaktawat <yashaswinisinghshaktawat@gmail.com>
2024-12-01 12:59:06 +0530

    add boilerplate

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,16 +1,12 @@
-/// <reference types="vitest" />
-
-import { defineConfig } from 'vite';
+import path from 'path';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'happy-dom',
-    testTimeout: 20000, // 20 seconds for puppeteer CI
-    // modified import('vitest/dist/config.js').defaultInclude
-    include: '**/*.{spec,e2e}.?(c|m)[jt]s?(x)',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 });
